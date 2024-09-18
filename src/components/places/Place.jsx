23fac3ae5403651cef302a5bfa666945832c1c07
ksa607 <karine.samyn@hogent.be>
2024-09-18 14:01:06 +0200

    Add delete place

--- src/components/places/Place.jsx
+++ src/components/places/Place.jsx
@@ -1,10 +1,13 @@
 // src/components/places/Place.jsx
-const Place = ({ id, name, rating }) => {
+const Place = ({ id, name, rating, onDelete = (f) => f }) => {
+  const handleDelete = () => {
+    onDelete(id);
+  };
   return (
     <div className='card bg-light border-dark mb-4'>
       <div className='card-body'>
         <h5 className='card-title'>{name}</h5>
-        <button className='btn btn-primary'>Verwijder</button>
+        <button className='btn btn-primary' onClick={handleDelete}>Verwijder</button>
       </div>
     </div>
   );
--- src/components/places/PlacesList.jsx
+++ src/components/places/PlacesList.jsx
@@ -1,23 +1,32 @@
 // src/components/places/PlacesList.jsx
+import { useState } from 'react';
 import { PLACE_DATA } from '../../api/mock_data';
 import Place from './Place';
 
 const PlacesList = () => {
-  const places = PLACE_DATA;
+  const [places, setPlaces] = useState(PLACE_DATA);
+
+  const handleDeletePlace = (id) => {
+    setPlaces((places) => places.filter((p) => p.id !== id));
+  };
+
   return (
-    <div className='grid mt-3'>
-      <div className='row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xxl-4 g-3'>
-        {places
-          .sort((a, b) =>
-            a.name.toUpperCase().localeCompare(b.name.toUpperCase()),
-          )
-          .map((p) => (
-            <div className='col' key={p.id}>
-              <Place {...p} />
-            </div>
-          ))}
+    <>
+      <h1>Places</h1>
+      <div className='grid mt-3'>
+        <div className='row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xxl-4 g-3'>
+          {places
+            .sort((a, b) =>
+              a.name.toUpperCase().localeCompare(b.name.toUpperCase()),
+            )
+            .map((p) => (
+              <div className='col' key={p.id}>
+                <Place {...p} onDelete={handleDeletePlace} />
+              </div>
+            ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
