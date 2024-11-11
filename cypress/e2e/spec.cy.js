59615160778b718c6f9bd60c7f5ef1d94b9cb158
Thomas Aelbrecht <thomasaelbrecht@live.com>
2024-11-11 21:47:50 +0100

    Add general login test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,4 +3,8 @@ describe('General', () => {
     cy.visit('http://localhost:5173');
     cy.get('h1').should('exist');
   });
+
+  it('should login', () => {
+    cy.login('pieter.vanderhelst@hogent.be', '12345678');
+  });
 });
